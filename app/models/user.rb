2fbaae7ea77613b8fb455e5a564b5529b20b57de
manamin14 <151704924+manamin14@users.noreply.github.com>
2023-12-25 17:38:57 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
          validates :username,    presence:true
 
          has_many :prototypes
+         has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-
-  resources :prototypes,only:[:index,:new,:create]
+  resources :prototypes do
+  resources :comments,only: :create
+  end
+  resources :users, only: :show
+  resources :prototypes,only:[:index,:new,:create,:show,:edit,:destroy,:update]
 end
