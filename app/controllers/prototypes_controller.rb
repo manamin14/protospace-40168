dd1647dabb5be7ddf1f2184b4acf08e1b1380f6b
manamin14 <151704924+manamin14@users.noreply.github.com>
2023-12-26 14:29:55 +0900

    コントローラーの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -54,7 +54,7 @@ end
     end
   
     def move_to_index
-      unless user_signed_in? && current_user == @prototype.user
+      unless user_signed_in? 
         redirect_to action: :index
       end
 
